534dcdb215e92c9b822f84bf780460f32458c036
ahmadm712 <ahmadmuji2808@gmail.com>
2021-10-01 21:05:59 +0700

    menambahkan page wish_list v24

--- ecommerce_shamo/lib/pages/home/wish_list_page.dart
+++ ecommerce_shamo/lib/pages/home/wish_list_page.dart
@@ -4,11 +4,66 @@ import 'package:flutter/material.dart';
 class WishlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Wishlist Page ',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: colorBg1,
+        centerTitle: true,
+        title: Text('Favorite Shoes'),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishlist() {
+      return Expanded(
+          child: Container(
+        width: double.infinity,
+        color: colorBg3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/image_wishlist.png',
+              width: 74,
+            ),
+            SizedBox(
+              height: 23,
+            ),
+            Text(
+              ' You don\'t have dream shoes?',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 15, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Let\'s find your favorite shoes',
+              style: secondaryTextStyle,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                height: 44,
+                child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+                        backgroundColor: colorPrimary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12))),
+                    child: Text('Explore Store',
+                        style: priceTextStyle.copyWith(
+                            fontWeight: medium, fontSize: 16))))
+          ],
+        ),
+      ));
+    }
+
+    return Column(
+      children: [header(), emptyWishlist()],
     );
   }
 }
