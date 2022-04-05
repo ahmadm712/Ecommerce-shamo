00333d2910b0e7c73e70765be01cbfb0248b4428
ahmadm712 <ahmadmuji2808@gmail.com>
2022-04-06 06:40:40 +0700

    inisialiasi firebase di projek dan setup

--- ecommerce_shamo/lib/main.dart
+++ ecommerce_shamo/lib/main.dart
@@ -13,11 +13,14 @@ import 'package:ecommerce_shamo/provider/cart_provider.dart';
 import 'package:ecommerce_shamo/provider/products_provider.dart';
 import 'package:ecommerce_shamo/provider/transactions_provider.dart';
 import 'package:ecommerce_shamo/provider/wishlist_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
